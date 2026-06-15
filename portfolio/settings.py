d50f79499250f95fc21ac46f01620ed4570adf35
Ajay kumar <ajaykr27052003@gmail.com>
2026-06-15 09:00:14 +0530

    Fix email password

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ajaykr27052003@gmail.com'
-EMAIL_HOST_PASSWORD = 'mqcp zzzi leqn naun'  # ← Apna Gmail App Password yahan
+EMAIL_HOST_PASSWORD = 'mqcpzzzileqnnaun'  # ← Apna Gmail App Password yahan
